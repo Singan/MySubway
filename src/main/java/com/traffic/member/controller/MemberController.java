dcc4787c969a595d4312f9d8b7e000ea28cbe4b3
“js988174” <rudtjs4902@naver.com>
2023-11-24 13:12:16 +0900

    docs: Controller 설정

--- src/main/java/com/traffic/member/controller/MemberController.java
+++ src/main/java/com/traffic/member/controller/MemberController.java
@@ -2,25 +2,33 @@ package com.traffic.member.controller;
 
 import com.traffic.member.api.NaverUtil;
 import com.traffic.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/1.0/member")
 @Tag(name = "MemberController", description = "회원 서비스")
 @RequiredArgsConstructor
 public class MemberController {
+
     private final MemberService memberService;
     @PostMapping
     public void signUp(){
     }
 
-    @GetMapping
+    @Operation(summary = "회원 로그인", security = {@SecurityRequirement(name = "bearerAuth"), @SecurityRequirement(name = "basicAuth")})
+    @Parameters({
+            @Parameter(name = "signIn", example = "id, pw", description = "내용 설명", required = true)
+    })
+    @ResponseBody
+    @GetMapping(value = "signIn", produces = MediaType.APPLICATION_JSON_VALUE)
     public void signIn() throws Exception{
         System.out.println(memberService.getNaverAuthorizeUrl());
     }
